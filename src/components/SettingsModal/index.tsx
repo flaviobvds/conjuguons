e48585491490ca5496fcb8d8a5a2831bd6e2e4a0
flaviobvds <40229603+flaviobvds@users.noreply.github.com>
2023-02-26 16:52:52 -0400

    finished styling Settings modal

--- src/components/SettingsModal/index.tsx
+++ src/components/SettingsModal/index.tsx
@@ -38,28 +38,76 @@ export function SettingsModal({ isOpen, onRequestClose, settings, setSettings }:
                         Subjects
                     </h2>
 
-                    <OptionWithCheckBox option='Je'/>
-                    <OptionWithCheckBox option='Tu'/>
-                    <OptionWithCheckBox option='Il'/>
-                    <OptionWithCheckBox option='Elle'/>
-                    <OptionWithCheckBox option='Nous'/>
-                    <OptionWithCheckBox option='Vous'/>
-                    <OptionWithCheckBox option='Ils'/>
-                    <OptionWithCheckBox option='Elles'/>
+                    <OptionWithCheckBox option='Je' />
+                    <OptionWithCheckBox option='Tu' />
+                    <OptionWithCheckBox option='Il' />
+                    <OptionWithCheckBox option='Elle' />
+                    <OptionWithCheckBox option='Nous' />
+                    <OptionWithCheckBox option='Vous' />
+                    <OptionWithCheckBox option='Ils' />
+                    <OptionWithCheckBox option='Elles' />
                 </div>
 
                 <div className={styles.selectionContainer}>
                     <h2 className={styles.subTitle}>
                         Verb Tenses
                     </h2>
+
+                    <h3 className={styles.tenseCategory}>
+                        Indicatif
+                    </h3>
+                    <OptionWithCheckBox option='Présent' />
+                    <OptionWithCheckBox option='Passé Composé' />
+                    <OptionWithCheckBox option='Passé Simple' />
+                    <OptionWithCheckBox option='Imparfait' />
+                    <OptionWithCheckBox option='Plus-que-parfait' />
+                    <OptionWithCheckBox option='Futur Simple' />
+
+
+                    <h3 className={styles.tenseCategory}>
+                        Conditionnel
+                    </h3>
+                    <OptionWithCheckBox option='Présent' />
+
+
+                    <h3 className={styles.tenseCategory}>
+                        Subjonctif
+                    </h3>
+                    <OptionWithCheckBox option='Présent' />
+                    <OptionWithCheckBox option='Imparfait' />
                 </div>
 
                 <div className={styles.selectionContainer}>
                     <h2 className={styles.subTitle}>
                         Verbs
                     </h2>
+
+                    <div className={styles.radioOptionsWrapper}>
+                        <input type="radio" name='verbs' value={'top25verbs'} className={styles.radioButton} />
+                        <label className={styles.label}>Top 25 Verbs</label>
+                    </div>
+
+                    <div className={styles.radioOptionsWrapper}>
+                        <input type="radio" name='verbs' value={'top50verbs'} className={styles.radioButton} />
+                        <label className={styles.label}>Top 50 Verbs</label>
+                    </div>
+
+                    <div className={styles.radioOptionsWrapper}>
+                        <input type="radio" name='verbs' value={'top100verbs'} className={styles.radioButton} />
+                        <label className={styles.label}>Top 100 Verbs</label>
+                    </div>
+
+                    <div className={styles.radioOptionsWrapper}>
+                        <input type="radio" name='verbs' value={'top50verbs'} className={styles.radioButton} />
+                        <label className={styles.label}>All Verbs</label>
+                    </div>
+
                 </div>
             </div>
+            <div className={styles.saveSettingsButton}>
+                <button>Save</button>
+            </div>
+
         </Modal>
     )
 }
